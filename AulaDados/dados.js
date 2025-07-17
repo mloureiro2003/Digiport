c43baad49dcc277c7355af265239adc4f02c96c9
Torres Loureiro <maria.torres.loureiro@sap.com>
2025-07-17 14:53:50 -0300

    dados

---
+++ AulaDados/dados.js
@@ -0,0 +1,21 @@
+// javascript eh tipada dinamicamente, reconhece tipo da variavel automaticamente
+let idade = 21; // numero (não tem diferença entre int e float)
+let nome = "Maria"; // string, aspas simples, duplas ou crase
+let ativo = true; // booleano true ou false
+let nota; // undefined
+let cidade = null; // null
+// ponto e virgula não é obrigatório, mas é bom pq c, java e c++ pedem
+
+console.log("idade:", idade);
+console.log("nome: ", nome);
+console.log("ativo: ", ativo);
+console.log("nota: ", nota);
+console.log("cidade: ", cidade);
+
+// let -> declara variavel
+// const -> declara constante
+
+const niver = "30 de julho";
+let ano = 2025;
+
+console.log("niver: ", niver, "de", ano);
