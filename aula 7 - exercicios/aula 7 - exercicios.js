a3193bd88c6437740f820ab0399ee205084494f7
Torres Loureiro <maria.torres.loureiro@sap.com>
2025-07-28 15:33:47 -0300

    commit aula 7

---
+++ aula 7 - exercicios/aula 7 - exercicios.js
@@ -0,0 +1,49 @@
+const SalarioHora = 30
+let HorasTrabalhadas = 160
+
+let SalarioMensal = SalarioHora * HorasTrabalhadas
+
+console.log (`Salario mensal final foi de R$${SalarioMensal}`)
+
+/////////////////////////////////////////////////////////////////////////////
+
+const Valor = 200
+const Desconto = 10
+
+console.log (`O valor final pago foi de R$${Valor * (100-Desconto)/100}`)
+
+/////////////////////////////////////////////////////////////////////////////
+
+const IdadeMinima = 18
+let IdadeJulia = 17
+
+if (IdadeJulia >= IdadeMinima) {
+    console.log("Você pode criar uma conta!");
+} else {
+    console.log("Você ainda não tem idade suficiente!");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+let QuantItens = 22
+if (QuantItens%2 === 0){
+    console.log("O número é par")
+}
+
+else {
+    console.log("O número é impar")
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+let Chuva = true
+let GuardaChuva = false
+
+if (Chuva === true){
+    if (GuardaChuva === true){
+        console.log ("Larissa pode sair!")
+    }
+    else {
+        console.log ("Larissa não pode sair")
+    }
+}
